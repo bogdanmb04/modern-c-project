010a5b49ba9422a272b75a836e31e904d6fda7e0
njrrrr <eduard.ardeleanu@student.unitbv.ro>
2024-11-24 16:43:49 +0200

    Implemented Logger class with log level control

---
+++ BattleCityServer/Loggingg/logging (2).cpp
@@ -0,0 +1,32 @@
+module logging;
+
+import <format>;
+
+
+std::string_view LogLevelToString(Logger::Level level)
+{
+	switch (level)
+	{
+	using enum Logger::Level;
+	case Info:
+		return "Info";
+	case Warning:
+		return "Warning";
+	case Error:
+		return "Error";
+	default:
+		return "";
+	}
+}
+
+Logger::Logger(std::ostream& os, Logger::Level minimumLevel) :
+	m_os{ os },
+	m_minimumLevel{ minimumLevel }
+{
+	// Empty
+}
+
+void Logger::SetMinimumLogLevel(Level level)
+{
+	m_minimumLevel = level;
+}
