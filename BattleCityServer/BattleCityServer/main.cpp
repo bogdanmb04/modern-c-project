c7d0eabf77a6f8cf905119e846d3fac69b794f8f
Flavia0320 <ciocoiuflavia008@gmail.com>
2024-11-24 21:29:41 +0200

    add timer

--- BattleCityServer/BattleCityServer/main.cpp
+++ BattleCityServer/BattleCityServer/main.cpp
@@ -7,12 +7,33 @@ import <vector>;
 
 #include "database.h"
 #include "routing.h"
+#include <chrono>
+#include <thread>
+#include <iomanip>
 
 //TODO: add namespace game for every class
 
 
 int main()
 {
+	//TODO: optimize this code
+	auto start_time = std::chrono::high_resolution_clock::now();
+	for (int i = 1; i < 30; i++) {
+		auto current_time = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<int> elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time);
+
+		int hours = elapsed_seconds.count() / 3600;
+		int minutes = (elapsed_seconds.count() % 3600) / 60;
+		int seconds = elapsed_seconds.count() % 60;
+
+		std::cout << "\rTime: "
+			<< std::setfill('0') << std::setw(2) << hours << ":"
+			<< std::setfill('0') << std::setw(2) << minutes << ":"
+			<< std::setfill('0') << std::setw(2) << seconds
+			<< std::flush;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+
 	std::ofstream of("syslog.log", std::ios::app);
 	Logger logger(of);
 	logger.Log("Started Application...", Logger::Level::Info);
