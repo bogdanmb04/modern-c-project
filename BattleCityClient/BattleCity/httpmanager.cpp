991bd598ff6cb1f305e15b9716d08d949ee89a18
Flavia0320 <ciocoiuflavia008@gmail.com>
2024-12-08 20:30:16 +0200

    cleane up

--- BattleCityClient/BattleCity/httpmanager.cpp
+++ BattleCityClient/BattleCity/httpmanager.cpp
@@ -36,27 +36,4 @@ void HttpManager::sendPostRequest(const QString& url, const QString& data, Respo
         callback(response);
     }
 }
-void HttpManager::sendShootRequest(const QString& playerID, int bulletDirection, ResponseCallback callback) {
-    QString shootData = QString("{\"playerID\":\"%1\", \"bulletDirection\": %2}").arg(playerID).arg(bulletDirection);
 
-    // Log the player ID and bullet direction
-    std::cout << "Sending shoot request: playerID = " << playerID.toStdString()
-        << ", bulletDirection = " << bulletDirection << std::endl;
-
-    cpr::Response response = cpr::Post(
-        cpr::Url{ "http://localhost:18080/shoot" },
-        cpr::Header{ {"Content-Type", "application/json"} },
-        cpr::Body{ shootData.toStdString() }
-    );
-
-    if (response.status_code == 200) {
-        std::cout << "Shoot request successful!\n";
-    }
-    else {
-        std::cout << "Shoot request failed with status code: " << response.status_code << '\n';
-    }
-
-    if (callback) {
-        callback(response);
-    }
-}
--- BattleCityClient/BattleCity/httpmanager.h
+++ BattleCityClient/BattleCity/httpmanager.h
@@ -14,7 +14,6 @@ public:
 
     void sendGetRequest(const QString& url, ResponseCallback callback);
     void sendPostRequest(const QString& url, const QString& data, ResponseCallback callback);
-    void sendShootRequest(const QString& playerID, int bulletDirection, ResponseCallback callback);
 
 };
 
