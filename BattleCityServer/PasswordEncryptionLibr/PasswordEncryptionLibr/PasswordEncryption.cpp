aac9d5f5bdca253768c10292f13e98f471041c8b
njrrrr <eduard.ardeleanu@student.unitbv.ro>
2024-12-13 22:02:23 +0200

    Implemented password encryption with a custom algorithm and random salt.

---
+++ BattleCityServer/PasswordEncryptionLibr/PasswordEncryptionLibr/PasswordEncryption.cpp
@@ -0,0 +1,42 @@
+#include "PasswordEncryption.h"
+#include <random>
+#include <sstream>
+#include <iomanip>
+
+/**
+ * Generates a random salt of fixed length.
+ */
+std::string generateSalt(size_t length) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, sizeof(charset) - 2);
+
+    std::string salt;
+    for (size_t i = 0; i < length; ++i) {
+        salt += charset[dis(gen)];
+    }
+    return salt;
+}
+
+/**
+ * Simple substitution-based encryption algorithm.
+ */
+std::string simpleEncrypt(const std::string& input, const std::string& salt) {
+    std::string encrypted;
+    for (size_t i = 0; i < input.size(); ++i) {
+        char encryptedChar = input[i] + salt[i % salt.size()];
+        encrypted += encryptedChar;
+    }
+    return encrypted;
+}
+
+std::string PasswordEncryptor::encryptPassword(const std::string& password) {
+    const size_t saltLength = 8; // Fixed salt length
+    std::string salt = generateSalt(saltLength);
+
+    std::string encryptedPassword = simpleEncrypt(password, salt);
+
+    // Append salt to encrypted password
+    return encryptedPassword + salt;
+}
