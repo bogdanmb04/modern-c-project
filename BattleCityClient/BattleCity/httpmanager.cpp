df836661f072071c52ed63430ea45f66d8b38f1b
njrrrr <eduard.ardeleanu@student.unitbv.ro>
2025-01-16 18:33:51 +0200

    modified send post request

--- BattleCityClient/BattleCity/httpmanager.cpp
+++ BattleCityClient/BattleCity/httpmanager.cpp
@@ -19,3 +19,21 @@ void HttpManager::sendGetRequest(const QString& url, ResponseCallback callback) 
     }
 }
 
+void HttpManager::sendPostRequest(const QString& url, const QString& data, ResponseCallback callback) {
+    cpr::Response response = cpr::Post(
+        cpr::Url{ url.toStdString() },
+        cpr::Body{ data.toStdString() }
+    );
+
+    if (response.status_code == 200 || response.status_code == 201) {
+        std::cout << "POST request successful!\n";
+    }
+    else {
+        std::cout << "POST request failed with status code: " << response.status_code << '\n';
+    }
+
+    if (callback) {
+        callback(response);
+    }
+}
+
