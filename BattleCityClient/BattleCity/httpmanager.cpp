639cbe46470767ed83c827f77ee36c18bb7050bf
njrrrr <eduard.ardeleanu@student.unitbv.ro>
2025-01-16 18:34:27 +0200

    sendgetrequest

--- BattleCityClient/BattleCity/httpmanager.cpp
+++ BattleCityClient/BattleCity/httpmanager.cpp
@@ -5,19 +5,6 @@ HttpManager::HttpManager() {}
 
 HttpManager::~HttpManager() {}
 
-void HttpManager::sendGetRequest(const QString& url, ResponseCallback callback) {
-    cpr::Response response = cpr::Get(cpr::Url{ url.toStdString() });
-    if (response.status_code == 200) {
-        std::cout << "GET request successful!\n";
-    }
-    else {
-        std::cout << "GET request failed with status code: " << response.status_code << '\n';
-    }
-
-    if (callback) {
-        callback(response);
-    }
-}
 
 void HttpManager::sendPostRequest(const QString& url, const QString& data, ResponseCallback callback) {
     cpr::Response response = cpr::Post(
