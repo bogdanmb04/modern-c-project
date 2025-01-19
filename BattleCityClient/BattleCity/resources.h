e283cd6562d9433ceee0b34c5bd561d0392eb8bb
Flavia0320 <ciocoiuflavia008@gmail.com>
2025-01-19 20:39:04 +0200

    draft

---
+++ BattleCityClient/BattleCity/resources.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+
+namespace Constants
+{
+    inline constexpr const char* kPoliceOfficer = ":/BattleCity/images/PoliceOfficer.png";
+    inline constexpr const char* kPoliceOfficer2 = ":/BattleCity/images/PoliceOfficer2.png";
+    inline constexpr const char* kPoliceOfficer3 = ":/BattleCity/images/PoliceOfficer3.png";
+    inline constexpr const char* kPoliceOfficer4 = ":/BattleCity/images/PoliceOfficer4.png";
+
+    inline constexpr const char* kZombie = ":/BattleCity/images/Zombie_Type1.png";
+    inline constexpr const char* kZombie1 = ":/BattleCity/images/Zombie1.png";
+    inline constexpr const char* kZombie2 = ":/BattleCity/images/Zombie2.png";
+    inline constexpr const char* kZombie3 = ":/BattleCity/images/Zombie3.png";
+
+    inline constexpr const char* kIntro = ":/BattleCity/images/Intro2.png";
+    inline constexpr const char* kBack = ":/BattleCity/images/Back.png";
+
+    inline constexpr const char* kBreakableWall = ":/BattleCity/images/BreakableWall_Type1.png";
+    inline constexpr const char* kUnbreakableWall = ":/BattleCity/images/UnbreakableWall_Type1.png";
+    inline constexpr const char* kPath = ":/BattleCity/images/Path.png";
+    inline constexpr const char* kBomb = ":/BattleCity/images/Bomb.png";
+
+    inline constexpr const char* kBeer = ":/BattleCity/images/Beer.png";
+    inline constexpr const char* kInvisible = ":/BattleCity/images/Invisible.png";
+    inline constexpr const char* kWallBomb = ":/BattleCity/images/BombBall_Type1.png";
+    inline constexpr const char* kPowerup = ":/BattleCity/images/Powerup.png";
+
+    inline constexpr const char* kCoin = ":/BattleCity/images/Coins.png";
+    inline constexpr const char* kSpecialMoney = ":/BattleCity/images/SpecialMoney.png";
+    inline constexpr const char* kUpgradeIcon = ":/BattleCity/images/UpgradeIcon.png";
+
+
+
+}
